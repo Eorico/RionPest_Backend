3aab8d1364fe118830ae5b238d1e76342498b275
Eorico Gonzales <eoricogonzales@gmail.com>
2026-02-23 21:13:51 +0800

    update: refactor the inventory inputs

--- app/test/inventorytest/testInventory.py
+++ app/test/inventorytest/testInventory.py
@@ -1,48 +1,57 @@
 import pytest
-from datetime import date
+from datetime import date, time
 
 def testAddInventory(client):
     response = client.post(
         "/inventory/",
         json={
-        "techname": "Tech1",
-        "chemname": "ChemicalA",
-        "usageLt" : 5.0,
-        "recDate": str(date.today())
+        "treatmentDate": str(date.today()),
+        "clientName":  "Client A",
+        "startTime": time,
+        "endTime" : time,
+        "chemicalName": "Chemical A",
+        "actualChemicalOnHand": 15.0
         }
     )
     
     assert response.status_code == 200
     data = response.json()
-    assert data["techName"] == "Tech1"
-    assert data["chemName"] == "ChemicalA"
-    assert data["usageLt"] == 5.0
+    assert data["clientName"] == "Client A"
+    assert data["chemicalName"] == "Chemical A"
+    assert data["actualChemicalOnHand"] == 15.0
+    assert data["treatmentDate"] == str(date.today())
+    assert data["startTime"] == time
+    assert data["endTime"] == time
     
 def testUpdateInventory(client):
     addResp = client.post(
         "/inventory/",
         json={
-        "techname": "Tech2",
-        "chemname": "ChemicalB",
-        "usageLt" : 3.0,
-        "recDate": str(date.today())
+        "treatmentDate": str(date.today()),
+        "clientName":  "Client B",
+        "startTime": time,
+        "endTime" : time,
+        "chemicalName": "Chemical B",
+        "actualChemicalOnHand": 20.0
         }
     )
     
     recordId = addResp.json()["id"]
     
-    updateResp = client.put(f"/inventory/{recordId}?usageLt=7.5")
+    updateResp = client.put(f"/inventory/{recordId}?actualChemcialOnHand=12.5")
     assert updateResp.status_code == 200
-    assert updateResp.json()["usageLt"] == 7.5
+    assert updateResp.json()["actualChemcialOnHand"] == 12.5
     
 def testDeleteInventory(client):
     addResp = client.post(
         "/inventory/",
         json={
-        "techname": "Tech1",
-        "chemname": "ChemicalA",
-        "usageLt" : 5.0,
-        "recDate": str(date.today())
+        "treatmentDate": str(date.today()),
+        "clientName":  "Client C",
+        "startTime": time,
+        "endTime" : time,
+        "chemicalName": "Chemical C",
+        "actualChemicalOnHand": 10.0
         }
     )
     recordId = addResp.json()["id"]
